95f282c118159b8b9555e36e975d1b0d28c21644
pablito <pabloivanjuarez@gmail.com>
2020-12-05 07:53:46 -0800

    home page dir created

---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './style.module.css';
+
+function Home() {
+  return (
+    <div className={classes.homeClass}>
+      HOME!
+    </div>
+  )
+}
+
+export default Home
