e6b62e8f678a9c13207637db8a6fdc1cfea13534
pablito <pabloivanjuarez@gmail.com>
2020-12-30 15:53:11 -0800

    App dependencies updated, path layed out.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './pages/home';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Play from './pages/Play';
+import Work from './pages/Work';
+import Eat from './pages/Eat';
 import Navbar from './components/Navbar';
-import About from './pages/about';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -16,6 +20,18 @@ function App() {
           </Route>
           <Route exact path="/about">
             <About />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/eat">
+            <Eat />
+          </Route>
+          <Route exact path="/play">
+            <Play />
+          </Route>
+          <Route exact path="/work">
+            <Work />
           </Route>
         </Switch>
         {/* <Footer /> */}
